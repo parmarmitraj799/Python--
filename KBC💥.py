569394e250607c30ea636b116436f7906d192b14
MITRAJ_Sinh_Parmar_07 <parmarmitraj687@gmail.com>
2026-09-06 15:38:38 +0530

    Create KBC💥.py This is a KBC game 🎮 (is not working)

---
+++ KBC💥.py
@@ -0,0 +1,17 @@
+a = input("Ente the name :")
+
+print(f"Welcome {a} in Quiz \n")
+
+Q = ['What is th 2+2 ','Who is spelin (dog)','What is the Capital of India']
+
+a =[4,'Dog','Dehli']
+
+for i in Q:
+    print(i)
+    b= input("Enter the Answer:")
+    
+    if b==a[1]:
+        print('You are Right')
+        
+    else:
+        print("by Ga")
